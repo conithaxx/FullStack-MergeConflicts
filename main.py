7a700082c1d6d7c6a204b3703748ff1b6db94908
{sivled26} <{santiago26delvis@gmail.com}>
2026-04-15 12:07:13 -0400

    Limpieza de nombres

--- main.py
+++ main.py
@@ -3,7 +3,18 @@
     Recibe una lista de nombres y debe devolver una lista 
     con los nombres limpios (sin espacios extra) y en formato correcto.
     """
-    return [n.strip().capitalize() for n in lista_nombres if n.strip()]
+    procesados = []
+
+    for nombre in lista_nombres:
+        # 1. Eliminar espacios en blanco al inicio y final
+        limpio = nombre.strip()
+
+        # 2. Poner la primera letra en mayúscula y el resto en minúscula
+        if limpio:  # 3. Solo agregar si no está vacío
+            procesados.append(limpio.capitalize())
+
+    return procesados
+
 
 if __name__ == "__main__":
     nombres_sucios = ["  juan", "ALICIA", " ", "  rOberto  ", "", "   ", "cRisToBal ", "AgustinA"]
